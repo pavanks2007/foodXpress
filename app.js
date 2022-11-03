f16f082e0ce56b3d8c41f7a248c74474f4b508ac
pavankumarprakashsavanur <pavanks2007@gmail.com>
2022-11-02 20:48:11 -0400

    Added js files for all user types

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const indexRouter = require('./routes/index');
+const customerRouter = require('./routes/customer');
+const managerRouter = require('./routes/manager');
+const adminRouter = require('./routes/admin');
+const driverRouter = require('./routes/driver');
 
 // Set path to views directory
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +18,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/customer', customerRouter);
+app.use('/manager', managerRouter);
+app.use('/admin', adminRouter);
+app.use('/driver', driverRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
