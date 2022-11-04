71eb85a5c13244c2c8818f7327212fef2279f99f
pavankumarprakashsavanur <pavanks2007@gmail.com>
2022-11-04 14:00:38 -0400

    Aded driver routes

--- routes/customer.js
+++ routes/customer.js
@@ -4,14 +4,18 @@ const router = express.Router();
 const constants = require('./constants.js');
 const dynamo = require('./dynamo.js')
 const ddbQueries = require('./query.js');
+const fs = require('fs');
 
 const ddb = dynamo.getDynamoDbClient();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const restaurants = await dynamo.queryTable(ddb, ddbQueries.queryListOfRestaurants());
-  console.log(restaurants);
   res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
+});
+
+router.post('/restaurants', async function(req, res, next) {
+  const restaurants = await dynamo.queryTable(ddb, ddbQueries.queryListOfRestaurants());
+  res.json(restaurants.Items);
 });
 
 module.exports = router;
