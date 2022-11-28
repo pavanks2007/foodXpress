8d9b3b165b5af2daf04f485fd799fa497744363c
pavankumarprakashsavanur <pavanks2007@gmail.com>
2022-11-28 13:00:57 -0500

    Fixes

--- routes/customer.js
+++ routes/customer.js
@@ -9,62 +9,71 @@ const fs = require('fs');
 const ddb = dynamo.getDynamoDbClient();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
+router.get('/', async function (req, res, next) {
+    res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
 });
 
-router.get('/restaurants', async function(req, res, next) {
-  const restaurants = await dynamo.queryTable(ddb, ddbQueries.queryListOfRestaurants());
-  res.json(restaurants.Items);
+router.get('/restaurants', async function (req, res, next) {
+    try {
+        const restaurants = await dynamo.queryTable(ddb, ddbQueries.queryListOfRestaurants());
+        res.json(restaurants.Items);
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to view restaurants', error: err });
+    }
 });
 
-router.post('/restaurant/items', async function(req, res, next) {
-  const {restaurant_id} = req.body;
-  const menuItems = await dynamo.queryTable(ddb, ddbQueries.queryMenuItemsInRestaurant(restaurant_id));
-  res.json(menuItems.Items);
+router.post('/restaurant/menu', async function (req, res, next) {
+    try {
+        const { restaurant_id } = req.body;
+        const menuItems = await dynamo.queryTable(ddb, ddbQueries.queryMenuItemsInRestaurant(restaurant_id));
+        res.json(menuItems.Items);
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to view restaurant menu', error: err });
+    }
 });
 
-router.post('/previous_orders', async function(req, res, next) {
-  const {customer_id} = req.body
-  const previous_orders = await dynamo.queryTable(ddb, ddbQueries.queryPreviousOrdersForCustomer(customer_id));
-  res.json(previous_orders.Items);
+router.post('/previous_orders', async function (req, res, next) {
+    const { customer_id } = req.body
+    const previous_orders = await dynamo.queryTable(ddb, ddbQueries.queryPreviousOrdersForCustomer(customer_id));
+    res.json(previous_orders.Items);
 });
 
-router.get('/getUserDetails/:id', async function(req, res, next) {
-  const user_id = req.params.id;
-  try {
-    const users = await dynamo.getFromTable(ddb, ddbQueries.queryGetCustomer(id));
-    res.json(users.Item);
-  } catch(err) {
-    console.error(err);
-    res.status(500).json({ err: 'Something went wrong', error: err});
-  }
+router.get('/getUserDetails/:id', async function (req, res, next) {
+    const user_id = req.params.id;
+    try {
+        const users = await dynamo.getFromTable(ddb, ddbQueries.queryGetCustomer(id));
+        res.json(users.Item);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong', error: err });
+    }
 });
 
-router.post('/addUser', async function(req, res, next) {
-  const {user_id, user_name, email, user_type, address} = req.body;
-  const createdAt = new Date().toString();
-  const encryptedCredential="Rutgers@123";
-  try {
-    const newCustomer = await dynamo.putInTable(ddb, ddbQueries.putCustomer(user_id, user_name, email, user_type, createdAt, address, encryptedCredential));
-    res.json({message: 'Successfully added user: ' + user_id});
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err });
-  }
+router.post('/addUser', async function (req, res, next) {
+    const { user_id, user_name, email, user_type, address } = req.body;
+    const createdAt = new Date().toString();
+    const encryptedCredential = "Rutgers@123";
+    try {
+        const newCustomer = await dynamo.putInTable(ddb, ddbQueries.putCustomer(user_id, user_name, email, user_type, createdAt, address, encryptedCredential));
+        res.json({ message: 'Successfully added user: ' + user_id });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+    }
 });
 
-router.post('/deleteUser', async function(req,res,next)
-{
-  const {user_id} = req.body
-  try {
-      const deleteUserQuery = ddbQueries.deleteUser(user_id);
-      const deleteUser = await dynamo.deleteInTable(ddb, deleteUserQuery);
-      res.json({message:'Successfully deleted user', query: deleteUserQuery, queryResult: deleteUser})
-  } catch(err) {
-      console.log(err)
-      res.send({message:'Unable to delete user', error: err})
-  }
+router.post('/deleteUser', async function (req, res, next) {
+    const { user_id } = req.body
+    try {
+        const deleteUserQuery = ddbQueries.deleteUser(user_id);
+        const deleteUser = await dynamo.deleteInTable(ddb, deleteUserQuery);
+        res.json({ message: 'Successfully deleted user', query: deleteUserQuery, queryResult: deleteUser })
+    } catch (err) {
+        console.log(err)
+        res.send({ message: 'Unable to delete user', error: err })
+    }
 });
 
 module.exports = router;
--- routes/manager.js
+++ routes/manager.js
@@ -2,37 +2,35 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const constants = require('./constants.js');
-const dynamo = require('./dynamo.js')
+const dynamo = require('./dynamo.js');
 const ddbQueries = require('./query.js');
 
 const ddb = dynamo.getDynamoDbClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
+router.get('/', function (req, res, next) {
+    res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
 });
 
-router.get('/login',(req,res)=>{
-  res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
+router.get('/login', (req, res) => {
+    res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
 })
 
 /*Takes in username and password*/
-router.post('/login',(req,res) => {
+router.post('/login', (req, res) => {
     //get user, send user to database, 
-    var user_name= req.body.user_name
+    var user_name = req.body.user_name
     console.log(user_name)
-    const exists=0; //exist=0 means does not exist
-   //username not found
-   if(exists==0)
-   {
-    return res.send('User does not exist!Try again.')
-   }
-  //if database has user, check if password matches.
+    const exists = 0; //exist=0 means does not exist
+    //username not found
+    if (exists == 0) {
+        return res.send('User does not exist!Try again.')
+    }
+    //if database has user, check if password matches.
     var password = req.body.password;
-    const match=0;
+    const match = 0;
 
-    if(match==0)
-    {
+    if (match == 0) {
         return res.send('Password is incorrect!Try again')
     }
 
@@ -41,148 +39,145 @@ router.post('/login',(req,res) => {
 })
 
 
-router.post('/dashboard',(req,res)=>
-{
+router.post('/dashboard', (req, res) => {
     //do later
 })
 
-router.get('/orders',(req,res)=>
-{
+router.get('/orders', (req, res) => {
     res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
 })
-//retreave all prev orders from database and send it over to webpage.
-router.post('/orders'),async function(req,res)
-{
-    const restaurantID="";
-    try
-    {
-        const allPrevOrders= await dynamo.queryTable(ddb, ddbQueries.queryPreviousOrdersForRestaurant(restaurantID));
-        console.log('Successfully pulled data')
-        res.json(allPrevOrders.Items);
-    }
-    catch(err)
-    {
-        console.log(err)
-        res.send('Unable to pull data.')
-    }
-}
 
-router.post('orders/confirm',(req,res)=>
-{
+router.post('orders/confirm', (req, res) => {
     //not sure how to handle this
 })
 
 //This will load page where restaurant menu will show up.
-router.get('/menu',(req,res)=>
-{
+router.get('/menu', (req, res) => {
     res.sendFile('users.html', { root: path.join(__dirname, '..', 'views') });
 })
 
 //Here we will pull the menu data and send it to frontend. 
-router.post('/menu2'),async function(req,res)
-{
-  const {restaurant_id, item_id, description, item_name, item_price} = req.body
-  console.log(restaurant_id, item_id, description, item_name, item_price)
-    try
-    {
-        const viewMenu= await dynamo.putInTable(ddb, ddbQueries.putMenu(restaurant_id, item_id, description, item_name, item_price))
+router.post('/menu2'), async function (req, res) {
+    const { restaurant_id, item_id, description, item_name, item_price } = req.body
+    console.log(restaurant_id, item_id, description, item_name, item_price)
+    try {
+        const viewMenu = await dynamo.putInTable(ddb, ddbQueries.putMenu(restaurant_id, item_id, description, item_name, item_price))
         console.log(viewMenu.Items)
-        res.json({message:'Successfully pulled Menu', data: viewMenu.Items})
+        res.json({ message: 'Successfully pulled Menu', data: viewMenu.Items })
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err)
         res.send('Unable to pull Menu')
     }
 }
 
 //Here we will update an item in the Menu
-router.post('/menu'), async function(req,res)
-{
+router.post('/menu'), async function (req, res) {
     //need help
 }
 
-router.get('/logout',  function (req, res)  
-{
+router.get('/logout', function (req, res) {
     // If the user is loggedin
     if (req.session.loggedin) {
-          req.session.loggedin = false;
-          res.redirect('/');
+        req.session.loggedin = false;
+        res.redirect('/');
     } else {
         // Not logged in
         res.redirect('/');
     }
-})
-
-router.post('/addMenuItem', async function(req,res,next){
-  const {restaurant_id, item_id, item_name, item_price, description} = req.body
-  try {
-      const addMenuItemQuery = ddbQueries.putMenuItemInRestaurant(restaurant_id, item_id, item_name, item_price, description);
-      const addMenuItem = await dynamo.putInTable(ddb, addMenuItemQuery);
-      res.json({message:'Successfully put menu item', query: addMenuItemQuery, queryResult: addMenuItem})
-  } catch(err) {
-      console.log(err)
-      res.send({message:'Unable to add menu item', error: err})
-  }
 });
 
-router.post('/deleteMenuItem', async function(req,res,next){
-  const {restaurant_id, item_id} = req.body
-  try {
-      const deleteMenuQuery = ddbQueries.deleteMenuItem(restaurant_id, item_id);
-      const deleteMenuItem = await dynamo.deleteInTable(ddb, deleteMenuQuery);
-      res.json({message:'Successfully deleted menu item', query: deleteMenuQuery, queryResult: deleteMenuItem})
-  } catch(err) {
-      console.log(err)
-      res.send({message:'Unable to delete menu item', error: err})
-  }
-}); 
-
-router.post('/updateRestaurantDetail', async function(req,res,next){
-    const {restaurant_id, key, value} = req.body
+router.post('/menu', async function (req, res, next) {
     try {
+        const { restaurant_id } = req.body;
+        const menuItems = await dynamo.queryTable(ddb, ddbQueries.queryMenuItemsInRestaurant(restaurant_id));
+        res.json(menuItems.Items);
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to view restaurant menu', error: err });
+    }
+});
+
+router.post('/orders', async function (req, res, next) {
+    try {
+        const { restaurant_id } = req.body;
+        const previous_orders = await dynamo.queryTable(ddb, ddbQueries.queryPreviousOrdersForRestaurant(restaurant_id));
+        res.json(previous_orders.Items);
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to view previous orders', error: err });
+    }
+});
+
+router.post('/addMenuItem', async function (req, res, next) {
+    try {
+        const { restaurant_id, item_id, item_name, item_price, description } = req.body;
+        const addMenuItemQuery = ddbQueries.putMenuItemInRestaurant(restaurant_id, item_id, item_name, item_price, description);
+        const addMenuItem = await dynamo.putInTable(ddb, addMenuItemQuery);
+        res.json({ message: 'Successfully put menu item', query: addMenuItemQuery, queryResult: addMenuItem });
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to add menu item', error: err });
+    }
+});
+
+router.post('/deleteMenuItem', async function (req, res, next) {
+    try {
+        const { restaurant_id, item_id } = req.body;
+        const deleteMenuQuery = ddbQueries.deleteMenuItem(restaurant_id, item_id);
+        const deleteMenuItem = await dynamo.deleteInTable(ddb, deleteMenuQuery);
+        res.json({ message: 'Successfully deleted menu item', query: deleteMenuQuery, queryResult: deleteMenuItem });
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to delete menu item', error: err });
+    }
+});
+
+router.post('/updateRestaurantDetail', async function (req, res, next) {
+    try {
+        const { restaurant_id, key, value } = req.body;
         const updateRestaurantDetailQuery = ddbQueries.updateRestaurantDetail(restaurant_id, key, value);
         const updateRestaurantDetail = await dynamo.updateTable(ddb, updateRestaurantDetailQuery);
-        res.json({message:'Successfully updated restaurant detail', query: updateRestaurantDetailQuery, queryResult: updateRestaurantDetail})
-    } catch(err) {
-        console.log(err)
-        res.send({message:'Unable to update restaurant detail', error: err})
+        res.json({ message: 'Successfully updated restaurant detail', query: updateRestaurantDetailQuery, queryResult: updateRestaurantDetail });
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to update restaurant detail', error: err });
     }
 });
 
-router.post('/addCoupon', async function(req,res,next){
-const {restaurant_id, coupon_id, coupon_value, expiration_time} = req.body
-try {
-    const addCouponQuery = ddbQueries.putCoupon(restaurant_id, coupon_id, coupon_value, true, expiration_time);
-    const addCoupon = await dynamo.putInTable(ddb, addCouponQuery);
-    res.json({message:'Successfully put coupon', query: addCouponQuery, queryResult: addCoupon})
-} catch(err) {
-    console.log(err)
-    res.send({message:'Unable to add coupon', error: err})
-}
+router.post('/addCoupon', async function (req, res, next) {
+    try {
+        const { restaurant_id, coupon_id, coupon_value, expiration_time } = req.body;
+        const addCouponQuery = ddbQueries.putCoupon(restaurant_id, coupon_id, coupon_value, true, expiration_time);
+        const addCoupon = await dynamo.putInTable(ddb, addCouponQuery);
+        res.json({ message: 'Successfully put coupon', query: addCouponQuery, queryResult: addCoupon });
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to add coupon', error: err });
+    }
 });
 
-router.post('/deleteCoupon', async function(req,res,next){
-    const {restaurant_id, coupon_id} = req.body
+router.post('/deleteCoupon', async function (req, res, next) {
     try {
+        const { restaurant_id, coupon_id } = req.body;
         const deleteCouponQuery = ddbQueries.deleteCoupon(restaurant_id, coupon_id);
         const deleteCoupon = await dynamo.deleteInTable(ddb, deleteCouponQuery);
-        res.json({message:'Successfully deleted coupon', query: deleteCouponQuery, queryResult: deleteCoupon})
-    } catch(err) {
-        console.log(err)
-        res.send({message:'Unable to delete coupon', error: err})
+        res.json({ message: 'Successfully deleted coupon', query: deleteCouponQuery, queryResult: deleteCoupon });
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to delete coupon', error: err });
     }
 });
 
-router.post('/viewCoupons', async function(req,res,next){
-    const {restaurant_id} = req.body
+router.post('/viewCoupons', async function (req, res, next) {
     try {
+        const { restaurant_id } = req.body;
         const viewCouponsQuery = ddbQueries.queryCouponsForRestaurant(restaurant_id);
         const viewCoupons = await dynamo.queryTable(ddb, viewCouponsQuery);
-        res.json(viewCoupons.Items)
-    } catch(err) {
-        console.log(err)
-        res.send({message:'Unable to retrieve coupons', error: err})
+        res.json(viewCoupons.Items);
+    } catch (err) {
+        console.log(err);
+        res.send({ message: 'Unable to retrieve coupons', error: err });
     }
 });
 
