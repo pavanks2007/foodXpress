b00c7cdcba23860d59d104684d14ea75f7ff3268
pavankumarprakashsavanur <pavanks2007@gmail.com>
2022-11-28 19:31:49 -0500

    Indentations

--- routes/index.js
+++ routes/index.js
@@ -7,86 +7,75 @@ const ddbQueries = require('./query.js');
 
 const ddb = dynamo.getDynamoDbClient();
 
-loggedIn=false;
-var userType='';
+loggedIn = false;
+var userType = '';
 
 /* GET home page. */
-router.get('/', (req, res) =>
-{
-  
-  if(!loggedIn)
-  {
-    res.redirect('login')
-  }
-  else 
-  {
-    res.redirect(userType)
-  }
+router.get('/', (req, res) => {
+
+    if (!loggedIn) {
+        res.redirect('login')
+    }
+    else {
+        res.redirect(userType)
+    }
 
 });
 
-router.get('/login',(req,res)=>
-{
-  res.render('general/generalLogin.ejs', { root: path.join(__dirname, '..', 'views') });
+router.get('/login', (req, res) => {
+    res.render('general/generalLogin.ejs', { root: path.join(__dirname, '..', 'views') });
 })
 
-router.post('/login',async function(req,res)
-{
-    const user_id= req.body.user_id
-    const password=req.body.password
-    try
-    {
-        const credentials= await dynamo.getFromTable(ddb,ddbQueries.getUserCredentials(user_id))
-        const userInfo=await dynamo.getFromTable(ddb,ddbQueries.getUserDetails(user_id))
-        userType=userInfo.Item[constants.USER_TYPE]
-        
-            if(password==credentials.Item[constants.ENCRYPTED_CREDENTIAL])
-            {
-              console.log("User successfully logged in.")
-              loggedIn=true;
-              res.redirect('/'+userType+'/dashboard') 
-            }
-            else
-            {
-              console.log('Wrong Password')
-              res.redirect('/')
-            }
+router.post('/login', async function (req, res) {
+    const user_id = req.body.user_id
+    const password = req.body.password
+    try {
+        const credentials = await dynamo.getFromTable(ddb, ddbQueries.getUserCredentials(user_id))
+        const userInfo = await dynamo.getFromTable(ddb, ddbQueries.getUserDetails(user_id))
+        userType = userInfo.Item[constants.USER_TYPE]
+
+        if (password == credentials.Item[constants.ENCRYPTED_CREDENTIAL]) {
+            console.log("User successfully logged in.")
+            loggedIn = true;
+            res.redirect('/' + userType + '/dashboard')
+        }
+        else {
+            console.log('Wrong Password')
+            res.redirect('/')
+        }
     }
-    
-    catch(err)
-    {
+
+    catch (err) {
         console.log(err)
         console.log('Wrong User Name or User does not exist.')
         res.redirect('/')
     }
 })
 
-router.get('/logout',  function (req, res)  
-{
+router.get('/logout', function (req, res) {
     loggedIn = false;
     res.redirect('/');
     console.log('User Successfully logged Out')
 })
 
-router.get('/register',(req,res)=>
-{
- 
-  res.sendFile('register.html', { root: path.join(__dirname, '..', 'views') });
+router.get('/register', (req, res) => {
+
+    res.sendFile('register.html', { root: path.join(__dirname, '..', 'views') });
 
 })
 
 
-router.post('/addUser', async function(req, res, next) {
-  const {user_id, user_name, email, user_type, address} = req.body;
-  const createdAt = new Date().toString();
-  const encryptedCredential="Rutgers@123";
-  try {
-    const newCustomer = await dynamo.putInTable(ddb, ddbQueries.putCustomer(user_id, user_name, email, user_type, createdAt, address, encryptedCredential));
-    res.json({message: 'Successfully added user: ' + user_id});
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err });
-  }
+router.post('/addUser', async function (req, res, next) {
+    const { user_id, user_name, email, user_type, address } = req.body;
+    const createdAt = new Date().toString();
+    const encryptedCredential = "Rutgers@123";
+    try {
+        const newCustomer = await dynamo.putInTable(ddb, ddbQueries.putCustomer(user_id, user_name, email, user_type, createdAt, address, encryptedCredential));
+        res.json({ message: 'Successfully added user: ' + user_id });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err });
+    }
 });
 
 /* GET home page. */
@@ -121,8 +110,7 @@ router.post('/restaurant/menu', async function (req, res, next) {
     }
 });
 
-router.get('/contact-us',(req,res)=>
-{
+router.get('/contact-us', (req, res) => {
     res.sendFile('general/contact-us-page.html', { root: path.join(__dirname, '..', 'views') });
 })
 
