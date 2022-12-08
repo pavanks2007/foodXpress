bad339b88ef8f61937b7a42494cd23f0554265fa
Edwin Vargas <egv15@scarletmail.rutgers.edu>
2022-12-07 23:37:15 -0500

    Registration fully working, Implemented Address Validation and converts address to coordinates. Registration front end and backend are fully integrated.

--- routes/index.js
+++ routes/index.js
@@ -155,7 +155,7 @@ router.post('/add_updateAddress/:rID', async function (req, res)
                 console.log('successfully Added/Updated Users Address')
                 try
                 {
-                    let url2=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(fulladd)}&key=AIzaSyCSCk3BE2UzEdCR0-NcWzmnD2dTCv3Jcsg`
+                    let url2=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(fulladd)}&key={delete curly brackets and add key}`
     
                     fetch(url2)
                     .then(function(response){
